bba525073cd2bc96889c80586604ccaf167f5341
Alexey Melezhik <melezhik@gmail.com>
2017-09-07 13:06:28 +0300

    print-tags

--- story.bash
+++ story.bash
@@ -1,6 +1,8 @@
 tag_key=$(config tag_key)
 tag_value=$(config tag_value)
 
+echo "list ec2 instances with tag: $tag_key => $tag_value"
+
 aws ec2 describe-instances --filters Name=tag:$tag_key,Values=$tag_value \
 --query 'Reservations[*].Instances[*].{ ID:InstanceId,St:State,Time:LaunchTime,IP:PrivateIpAddress,Tags:Tags }' | perl -n -MJSON -e '
 $json.=$_;
