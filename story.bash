70123d8546da04c26445aebd5e56660bc76f6b5f
Alexey Melezhik <melezhik@gmail.com>
2017-12-27 13:32:04 +0300

    show type

--- story.bash
+++ story.bash
@@ -4,7 +4,7 @@ tag_value=$(config tag_value)
 echo "list ec2 instances with tag: $tag_key => $tag_value"
 
 aws ec2 describe-instances --filters Name=tag:$tag_key,Values=$tag_value \
---query 'Reservations[*].Instances[*].{ ID:InstanceId,St:State,Time:LaunchTime,IP:PublicIpAddress,IP2:PrivateIpAddress,Tags:Tags }' | perl -n -MJSON -e '
+--query 'Reservations[*].Instances[*].{ ID:InstanceId,Type:InstanceType,St:State,Time:LaunchTime,IP:PublicIpAddress,IP2:PrivateIpAddress,Tags:Tags }' | perl -n -MJSON -e '
 $json.=$_;
   END {
     my $ii = decode_json($json);
@@ -15,13 +15,13 @@ $json.=$_;
 
 format STDOUT_TOP  =
 
-ID                        Public IP           Private IP          Time                  State
-----------------------------------------------------------------------------------------------------------
+ID                        Type            Public IP           Private IP          Time                  State
+----------------------------------------------------------------------------------------------------------------------------
 .
 
 format STDOUT  =
-@<<<<<<<<<<<<<<<<<<<<<<<< @<<<<<<<<<<<<<<<<<< @<<<<<<<<<<<<<<<<<< @>>>>>>>>>>>>>>>>>>   @<<<<<<<<<<<<<<<<< ~
-$i->{ID},  $i->{IP}||'Null', $i->{IP2}||'Null', $i->{Time}, join "/", ($i->{St}{Code}, $i->{St}{Name})
+@<<<<<<<<<<<<<<<<<<<<<<<< @<<<<<<<<<<<<<< @<<<<<<<<<<<<<<<<<< @<<<<<<<<<<<<<<<<<< @>>>>>>>>>>>>>>>>>>   @<<<<<<<<<<<<<<<<< ~
+$i->{ID},  $i->{Type}, $i->{IP}||'Null', $i->{IP2}||'Null', $i->{Time}, join "/", ($i->{St}{Code}, $i->{St}{Name})
 .
 
 '
